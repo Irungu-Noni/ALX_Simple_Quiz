4c729a0d29c9070ea99dcfb214ae4853f27aeaa6
Irungu-Noni <kamsycally@gmail.com>
2025-08-01 12:17:33 +0300

    Implementing a simple interactive quiz

---
+++ quiz.js
@@ -0,0 +1,20 @@
+document.addEventListener("DOMContentLoaded", function() {
+
+function checkAnswer() {
+    const correctAnswer = "4";
+
+    const selectedRadio = document.querySelector("input[name='quiz']:checked");
+
+    const userAnswer = selectedRadio ? selectedRadio.value : null;
+
+    const feedback = document.getElementById("feedback");
+
+    if (userAnswer === correctAnswer) {
+        feedback.textContent = "Correct! Well done.";
+    } else {
+        feedback.textContent = "That's not correct. Try again!.";
+    } 
+}
+    document.getElementById("submit-answer").addEventListener("click", checkAnswer);
+});
+    
